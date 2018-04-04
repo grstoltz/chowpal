14d2b748497b08c4840cd8835733628ac468095f
Grant Stoltz <grant@grantstoltz.com>
2018-04-03 19:58:50 -0700

    Initial file structure

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const path = require("path");
+const logger = require("morgan")
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {

a67fad3dafbed16b90f0d0a2e3bf518623ed89f6
Grant Stoltz <grant@grantstoltz.com>
2018-04-05 22:09:08 -0700

    Working AWS interaction

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const dotenv = require('dotenv').config()
 const express = require("express");
 const path = require("path");
 const logger = require("morgan")
@@ -7,17 +8,20 @@ const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(routes);
+
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(routes);
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
+} 
 
 // Send every request to the React app
 // Define any API routes before this runs

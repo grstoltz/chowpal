608dbeadff6799847aeccbfb2c533fbb06a6adb8
Grant Stoltz <grant@grantstoltz.com>
2018-04-05 22:15:21 -0700

    Revert "Working AWS interaction"
    This reverts commit a67fad3dafbed16b90f0d0a2e3bf518623ed89f6.

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,11 +2,9 @@ const router = require("express").Router();
 const foodRoutes = require("./food");
 const itemRoutes = require("./item");
 const userRoutes = require("./user");
-const awsRoutes = require("./aws")
 
 router.use("/food", foodRoutes);
 router.use("/item", itemRoutes);
 router.use("/user", userRoutes);
-router.use("/aws", awsRoutes)
 
 module.exports = router;

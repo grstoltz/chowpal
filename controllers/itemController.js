19c13bfd237f413bd801f51dc027c735eb61e7c1
Grant Stoltz <grant@grantstoltz.com>
2018-04-07 14:20:24 -0700

    Added receipt parsing

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,6 +1,6 @@
-// var models = require("../models");
-// var db = models.db
-// var Op = models.Op
+var models = require("../models");
+var db = models.db
+var Op = models.Op
 
 // Display list of all items.
 exports.findAll = function(req, res) {
@@ -10,11 +10,32 @@ exports.findAll = function(req, res) {
 // Display detail page for a specific item.
 exports.findOne = function(req, res) {
     res.send('NOT IMPLEMENTED: item detail: ' + req.params.id);
+    
 };
 
 // Handle item create on POST.
 exports.createOne = function(req, res) {
     res.send('NOT IMPLEMENTED: item create POST');
+};
+
+exports.findOrCreate = function(req, res) {
+    console.log(req.body.product_name)
+    // db.Items.findOrCreate({where: {
+    //     product_name: req.body.product_name
+    //         }, 
+    //     defaults: {
+    //         product_name: req.body.product_name,
+    //         store: req.body.store,
+    //         UPC: req.body.UPC,
+    //         product_id: req.body.product_id
+    //         }
+    //     }).spread((item, created) => {
+    //         console.log(item.get({
+    //             plain: true
+    //         }))
+    //         console.log(created)
+    //   });
+    res.send(req.body.product_name)
 };
 
 // Display item delete form on DELETE.

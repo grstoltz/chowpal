5cef21804da57e5cf7488dcc9a738020376d28a3
Abby Thoresen <akthoresen39@gmail.com>
2018-04-10 20:23:07 -0700

    nav

--- client/src/stories/index.js
+++ client/src/stories/index.js
@@ -9,14 +9,14 @@ import SelectButton from '../components/SelectButton/SelectButton.js';
 import Form from '../components/Form/Form.js';
 import List from '../components/List/List.js';
 import ChowCard from '../components/ChowCard/ChowCard.js';
-
+import ChowNav from '../components/ChowNav/ChowNav.js';
 
 storiesOf('Modal', module).add('login modal', () => <Modal />);
 storiesOf('SelectButton', module).add('Standard Button component', () => <SelectButton />);
 storiesOf('Form', module).add('MVP Form', () => <Form />);
 storiesOf('List', module).add('Early List Idea', () => <List />);
 storiesOf('ChowCard', module).add('Basic Card', () => <ChowCard />);
-
+storiesOf('ChowNav', module).add('Nav Bar', () => <ChowNav />);
 
 
 

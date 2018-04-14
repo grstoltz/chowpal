e1ad76389f4965aa270f57184fa1ca947977bc89
Trish Mederos <trishmederos@gmail.com>
2018-04-14 12:12:55 -0700

    formatted routers

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
-const path = require("path");
-const router = require("express").Router();
-const apiRoutes = require("./api");
+const path = require('path');
+const router = require('express').Router();
+const apiRoutes = require('./api');
 
 // API Routes
-router.use("/api", apiRoutes);
+router.use('/api', apiRoutes);
 
 router.get('/api/test', (req, res) => {
-  console.log('Hello')
+  console.log('Hello');
   res.json({
     message: 'Router testing',
-  })
-})
+  });
+});
 
 // If no API routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+router.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 module.exports = router;

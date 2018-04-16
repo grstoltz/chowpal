062fb1ae8e8029cbdb476faa24ed44d480481545
Grant Stoltz <grant@grantstoltz.com>
2018-04-16 10:53:30 -0700

    Frontend changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,12 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Item from "./pages/Item";
 import MaterialNav from "./components/ChowNav/MaterialNav";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper/Wrapper";
+import { Container } from 'mdbreact'
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 
 class App extends Component {

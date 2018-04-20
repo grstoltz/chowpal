bd8194a7dc0cbc1bad1fa930e7e016b8256732ba
Alan Drake <alangeorgedrake@gmail.com>
2018-04-19 21:50:54 -0700

    added in upload modal to nav, changed card modal

--- client/src/components/Modal/CardModal.js
+++ client/src/components/Modal/CardModal.js
@@ -1,42 +1,41 @@
 
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import MaterialButton from '../SelectButton/MaterialButton.js';
-import PantryCard from '../PantryCard/PantryCard.js';
+import MaterialButton from '../SelectButton/MaterialButton';
+import ModalCard from '../ModalCard/ModalCard';
 
-class LModal extends React.Component {
+class CardModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+    };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            modal: false
-        };
+    this.toggle = this.toggle.bind(this);
+  }
 
-        this.toggle = this.toggle.bind(this);
-    }
+  toggle() {
+    this.setState({
+      modal: !this.state.modal,
+    });
+  }
 
-    toggle() {
-        this.setState({
-            modal: !this.state.modal
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <Button color="primary" onClick={this.toggle}>{this.props.buttonLabel}</Button>
-                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                    <ModalHeader toggle={this.toggle}>{this.props.modalHead}</ModalHeader>
-                    <ModalBody>
-                       <PantryCard title='Item Name' text='Item description'></PantryCard>
-                    </ModalBody>
-                    <ModalFooter>
-                        <MaterialButton buttonColor='success' buttonText='Something about the item'></MaterialButton>
-                    </ModalFooter>
-                </Modal>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <MaterialButton buttonColor='white' buttonText='Hello' onClick={this.toggle} />
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>{this.props.modalHead}</ModalHeader>
+          <ModalBody>
+            <ModalCard title='Item Name' text='Item description' />
+          </ModalBody>
+          <ModalFooter>
+            <MaterialButton buttonColor='success' buttonText='Something about the item' />
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
 }
 
-export default LModal;
+export default CardModal;
--- client/src/stories/index.js
+++ client/src/stories/index.js
@@ -4,7 +4,7 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import 'bootstrap/dist/css/bootstrap.css';
-import  Modal  from '../components/Modal/Modal.js';
+import Modal  from '../components/Modal/Modal.js';
 import SelectButton from '../components/SelectButton/SelectButton.js';
 import Form from '../components/Form/Form.js';
 // import List from '../components/List/List.js';
@@ -15,6 +15,7 @@ import MaterialCard from '../components/ChowCard/MaterialCard.jsx';
 // import MaterialList from '../components/List/MaterialList.js';
 import MaterialButton from '../components/SelectButton/MaterialButton.js';
 import CardModal from '../components/Modal/CardModal.js';
+import MaterialNav from '../components/MaterialNav/MaterialNav.js';
 
 storiesOf('Modal', module).add('input modal', () => <Modal buttonLabel='Modal' modalHead='Receipt Upload' modalBody='Choose a file to upload by clicking the button below'/>);
 storiesOf('CardModal', module).add('card modal', () => <CardModal buttonLabel='Modal' modalHead='Card Title'/>);
@@ -27,6 +28,7 @@ storiesOf('ChowCard', module).add('Basic Card', () => <ChowCard />);
 storiesOf('MaterialCard', module).add('Card using Material', () => <MaterialCard />);
 // storiesOf('ChowNav', module).add('Nav Bar', () => <ChowNav />);
 storiesOf('Footer', module).add('Footsy', () => <Footer />);
+storiesOf('MaterialNav', module).add('trying to add modal', () => <MaterialNav />);
 
 
 
